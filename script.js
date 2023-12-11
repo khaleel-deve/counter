1d66b81298295f2ead64cf1f50231e1ea55c4cf9
khaleel-deve <151909499+khaleel-deve@users.noreply.github.com>
2023-12-11 12:47:00 +0530

    first commit
    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function fun1(){
+    let a = document.getElementById('counter').innerHTML
+    let value = parseInt(a) + 1
+     document.getElementById('counter').innerHTML=value
+}
+
+function fun2(){
+    let a =document.getElementById('counter').innerHTML
+    let value = parseInt(a)-1
+     document.getElementById('counter').innerHTML=value
+}
